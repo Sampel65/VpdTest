8649b23f2da2e1acda1b36d785cae9d2df7371a6
Sampel <sampeldan65@gmail.com>
2024-09-23 14:41:41 +0100

    Initial Commit

---
+++ VpdTest/VpdTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  VpdTestApp.swift
+//  VpdTest
+//
+//  Created by DevSampel on 23/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct VpdTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
